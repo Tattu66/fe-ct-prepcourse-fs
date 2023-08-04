c533be42bbba754dad94be9cca7df4005fd091a5
tattu66 <scleis66@gmail.com>
2023-08-04 13:15:24 -0300

    Ejercicios Resueltos, y corregidos <3

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -69,7 +69,18 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
-   
+   switch (letra.toLowerCase()) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+         return "Es vocal";
+         break;
+      default: 
+         return "Dato incorrecto";
+         break;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
